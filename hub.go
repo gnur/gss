ba0e123415ce2addf15a498b92ec4432715debe5
Erwin de Keijzer <git@0x8.be>
2012-10-28 20:20:53 +0100

    code cleanup to delete egdk specific bits

--- hub.go
+++ hub.go
@@ -1,13 +1,10 @@
 package main
 
-import (
-	"github.com/ziutek/mymysql/mysql"
-	_ "github.com/ziutek/mymysql/native"
-	"strings"
-)
+//import (
+//	"strings"
+//)
 
 type hub struct {
-	db         mysql.Conn
 	listeners  map[*connection]bool
 	command    chan usercmd
 	connect    chan *connection
@@ -22,11 +19,6 @@ var h = hub{
 }
 
 func (h *hub) run() {
-	h.db = mysql.New("tcp", "", "127.0.0.1:3306", "egdk", "nottheactualpassword", "egdk")
-	err := h.db.Connect()
-	if err != nil {
-		panic(err)
-	}
 	for {
 		select {
 		case c := <-h.disconnect:
@@ -47,27 +39,8 @@ func (h *hub) run() {
 			if !found || found {
 				h.listeners[c] = true
 			}
-		case m := <-h.command:
-			if m.fromuser && m.command[:3] == "np:" {
-				commands := strings.Split(m.command[3:], ":")
-				//INSERT INTO user_progress (user_id,video_publicid,position) VALUES (1,2,3) ON DUPLICATE KEY UPDATE position=121
-				stmt, err := h.db.Prepare("INSERT INTO user_progress (user_hash,video_publicid,position) VALUES (?,?,?) ON DUPLICATE KEY UPDATE position=?")
-				if err == nil {
-					stmt.Run(m.userid, commands[0], commands[1], commands[1])
-				} else {
-					h.db = mysql.New("tcp", "", "127.0.0.1:3306", "egdk", "nottheactualpassword", "egdk")
-					err := h.db.Connect()
-					if err != nil {
-						panic(err)
-					}
-				}
-			} else if !m.fromuser {
-				for val := range h.listeners {
-					if val.userid == m.userid {
-						val.send <- m.command
-					}
-				}
-			}
+		//case m := <-h.command:
+        //    incoming command?
 		}
 	}
 }
